23a47fa60bc4e14a7c63de95675f26474652c8ed
Azie Amiza <22FTT1497@student.pb.edu.bn>
2024-04-04 14:24:50 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ASG3-22FTT1497-MWE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
